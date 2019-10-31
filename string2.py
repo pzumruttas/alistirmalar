6ec782db43e84a6392c9393864cd00362b1c213d
pzumruttas <56044119+pzumruttas@users.noreply.github.com>
2019-10-31 22:50:11 +0300

    Update string2.py

--- string2.py
+++ string2.py
@@ -1,9 +1,9 @@
-girilen_string=str(input("Stringi giriniz:"))
+girilen=input("Stringi giriniz:")
 
-aranan_harf=str(input("Aranan harf:"))
+aranan_harf=input("Aranan harf:")
 a=0
-for i in range(len(girilen_string)):
-    if girilen_string[i]==aranan_harf:
+for i in range(len(girilen)):
+    if girilen[i]==aranan_harf:
         a+=1
 
 print(a)
